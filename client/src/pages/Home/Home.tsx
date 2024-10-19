dc5f3be9d288bba730843c1239e3b65ae3a6b6cb
seula <urghb12375@gmail.com>
2024-10-19 21:12:32 +0900

    feat-FE-SA/sidebar isopen 다른 파일에서 사용하기 위해 상태를 상위 컴포넌트로 끌어올리고 props로 상태 전달 구현

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,19 +1,17 @@
-
 import React, { useState } from "react";
 import SideBar from "../../components/SideBar/SideBar";
 import Chat from "../../components/Chat/Chat";
-import Board from "../../components/Board/Board";
 import "./Home.css";
 
 function Home() {
+  //상태 끌어올리기
+  const [isOpen, setIsOpen] = useState(false);
   //상태 끌어올리기
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="home">
       <SideBar isOpen={isOpen} setIsOpen={setIsOpen} />
       <Chat setIsOpen={setIsOpen} />
-      <div className="vertical-line"></div>
-      <Board />
     </div>
   );
 }
