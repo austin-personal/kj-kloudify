d8456142311fb51ec0d20139cc91089e7ce9504f
kim990401 <xogus1p@gmail.com>
2024-10-19 14:47:09 +0900

    FE-TH / simple NavBar

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -1,11 +1,21 @@
+// src/components/NavBar/NavBar.tsx
 import React from 'react';
-import './Header.css';
+import './NavBar.css';
 
-function Header() {
+const NavBar: React.FC = () => {
   return (
-    <div>
-    </div>
+    <nav className="navbar">
+      <div className="navbar-logo">
+        MyLogo
+      </div>
+      <ul className="navbar-links">
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/services">Services</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
   );
-}
+};
 
-export default Header;
+export default NavBar;
--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './Login.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from '../Home/Home'
 
 function Login() {
-  return (
-    <div>
-      
-    </div>
-  );
+    return (
+        <div>
+            <Router>
+                <button></button>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default Login;
