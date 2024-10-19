30a7e1653b907dad586737818296a4d33cfc55b5
kim990401 <xogus1p@gmail.com>
2024-10-19 15:51:33 +0900

    FE-TH / Login with simple css

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import LoginPage from '../../components/LoginPage/LoginPage';
 
 interface LoginProps {
     setIsLoggedIn: (loggedIn: boolean) => void;
 }
 
 const Login: React.FC<LoginProps> = ({ setIsLoggedIn }) => {
-    const navigate = useNavigate();
-
-    const handleLogin = () => {
-        setIsLoggedIn(true);
-        navigate('/');
-    };
-
-    return (
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '50px' }}>
-            <h1>Login</h1>
-            <button onClick={handleLogin}>Login</button>
-        </div>
-    );
+    return <LoginPage setIsLoggedIn={setIsLoggedIn} />;
 };
 
 export default Login;
