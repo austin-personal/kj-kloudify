333720bca5df18893d6f034c56e077ee302abaf7
seula <urghb12375@gmail.com>
2024-10-19 21:12:06 +0900

    feat-FE-SA/sidebar isopen 다른 파일에서 사용하기 위해 상태를 상위 컴포넌트로 끌어올리고 props로 상태 전달 구현

--- client/src/components/SideBar/SideBar.tsx
+++ client/src/components/SideBar/SideBar.tsx
@@ -8,13 +8,11 @@ import {
   faChevronLeft,
 } from "@fortawesome/free-solid-svg-icons";
 
-
 interface SidebarProps {
   isOpen: boolean;
   setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 const SideBar: React.FC<SidebarProps> = ({ isOpen, setIsOpen }) => {
-
   const toggleSidebar = () => {
     setIsOpen(!isOpen); // 현재의 isOpen 상태를 반대로 설정
   };
