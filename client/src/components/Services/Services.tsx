10164f5c5cbb895aa0938dce102b293720e03b12
seula <urghb12375@gmail.com>
2024-10-21 18:40:56 +0900

    feat-FE-SA/summary-price 모달 창 추가, 버튼 라우팅 추가

--- client/src/components/Services/Services.tsx
+++ client/src/components/Services/Services.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Services.css";
-
+import { useNavigate } from "react-router-dom";
 const Services: React.FC = () => {
+  // 모달 열림 상태 관리
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const navigate = useNavigate();
+  const handleDeploy = () => {
+    navigate("/profile");
+  };
+  const handleGuide = () => {
+    navigate("/guide");
+  };
+
+  // 모달 열고 닫는 함수
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
   return (
     <div className="services">
       <div className="price-summary-header">
@@ -48,16 +62,32 @@ const Services: React.FC = () => {
         </div>
       </div>
       <div className="middle-btn">
-        <button className="price-summary-btn">Price Summary</button>
+        <button className="price-summary-btn" onClick={toggleModal}>
+          Price Summary
+        </button>
+        {/* 모달이 열려 있을 때만 모달 컴포넌트 보여주기 */}
+        {isModalOpen && (
+          <div className="modal-overlay">
+            <div className="modal">
+              <h2>Price Summary Details</h2>
+              <p>This is the content of the modal window.</p>
+              <button className="close-btn" onClick={toggleModal}>
+                Close
+              </button>
+            </div>
+          </div>
+        )}
       </div>
       <hr />
       <h3>배포 전 마지막 단계</h3>
       <p>배포를 위한 AWS 관련 정보가 필요합니다.</p>
       <div className="right-btn">
-        <button className="info-btn">정보 입력하러 가기 →</button>
+        <button className="info-btn" onClick={handleGuide}>
+          정보 입력하러 가기 →
+        </button>
       </div>
       <div className="right-btn">
-        <button>Deploy</button>
+        <button onClick={handleDeploy}>Deploy</button>
       </div>
     </div>
   );
