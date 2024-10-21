3f734f3b34beda4251009ecc4bc67634cc1ee45a
kim990401 <xogus1p@gmail.com>
2024-10-21 12:08:02 +0900

    feat-TH / making guide-page

--- client/src/components/GuidePage/KeyForm.tsx
+++ client/src/components/GuidePage/KeyForm.tsx
@@ -1,37 +1,9 @@
-import React, { useState } from "react";
-import './KeyForm.css'
+import React from "react";
 
 const KeyForm: React.FC = () => {
-    const [keyId, setKeyId] = useState('');
-    const [secretKey, setSecretKey] = useState('');
-    const [keyPair, setKeyPair] = useState('');
-
     return (
-        <div className="key-form">
-            <div className="input-box">
-                <div>AWS_ACCESS_KEY_ID</div>
-                <input type="text"
-                    placeholder="ex)ABCD1234ABCD1234"
-                    value={keyId}
-                    onChange={(e) => setKeyId(e.target.value)}
-                />
-            </div>
-            <div className="input-box">
-                <div>AWS_SECRET_ACCESS_KEY</div>
-                <input type="text"
-                    placeholder="ex)ABCD1234ABCD1234"
-                    value={secretKey}
-                    onChange={(e) => setSecretKey(e.target.value)}
-                />
-            </div>
-            <div className="input-box">
-                <div>Key pairs</div>
-                <input type="text"
-                    placeholder="ex)배포에 필요한 공개 키 정보"
-                    value={keyPair}
-                    onChange={(e) => setKeyPair(e.target.value)}
-                />
-            </div>
+        <div>
+            <p>ㅎㅇ</p>
         </div>
     )
 }
