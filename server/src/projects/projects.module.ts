d83b8916f4f8cd530934b6bed284e31ee4a2e83d
changmo <kmaestro043@gmail.com>
2024-10-22 15:00:38 +0900

    feat-BE-MS/Update Users objects modules, controllers

--- server/src/projects/projects.module.ts
+++ server/src/projects/projects.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProjectsService } from './projects.service';
 import { ProjectsController } from './projects.controller';
 import { Projects } from './entity/projects.entity';
-import { Users } from '../users/entity/users.entity'; // User 엔티티 추가
+import { Users } from '../users/entity/users.entity'; 
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Projects, Users])],
+  imports: [TypeOrmModule.forFeature([Projects, Users]), UsersModule], 
   providers: [ProjectsService],
   controllers: [ProjectsController],
 })
