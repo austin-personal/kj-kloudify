d9e4577598f0ae55c5d2ee15553b7a62e6add5e1
kim990401 <xogus1p@gmail.com>
2024-10-22 21:42:23 +0900

    feat-TH / project-api finish

---
+++ client/src/services/projects.ts
@@ -0,0 +1,54 @@
+import axios from 'axios';
+
+// 태현 api 주소 확인!!!
+const API_URL = 'http://localhost:3000/Projects';
+
+export const create = async (projectName: string) => {
+    try {
+        // 태현 api 주소 확인!!!
+        const response = await axios.post(`${API_URL}/create`, {
+            projectName
+        });
+        return response;
+    } catch (error) {
+        console.error('프로젝트 생성 개박살!! : ', error);
+        throw error;
+    }
+};
+
+export const deleteProject = async (PID: number) => {
+    try {
+        // 태현 api 주소 확인!!!
+        const response = await axios.delete(`${API_URL}/delete`, {
+           data : {
+            PID
+           }
+        });
+        return response;
+    } catch (error) {
+        console.error('프로젝트 삭제 개박살!! :', error);
+        throw error;
+    }
+};
+
+export const projectOneInfo = async (pid: number) => {
+    try {
+        // 태현 api 주소 확인!!!
+        const response = await axios.get(`${API_URL}/${pid}`);
+        return response;
+    } catch (error) {
+        console.error('프로젝트 정보 개박살!! :', error);
+        throw error;
+    }
+};
+
+export const ProjectAllInfo = async () => {
+    try {
+        // 태현 api 주소 확인!!!
+        const response = await axios.get(`${API_URL}/findAll`);
+        return response;
+    } catch (error) {
+        console.error('프로젝트 정보 개박살!! :', error);
+        throw error;
+    }
+};
