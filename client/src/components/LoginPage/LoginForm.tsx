56719a052fe68b2953ef5aab8b80662de21b4bbd
seula <urghb12375@gmail.com>
2024-10-23 00:08:54 +0900

    feat-FE-SA/ 아이디->이메일 입력하세요로 로그인창 input placeholder 수정

--- client/src/components/LoginPage/LoginForm.tsx
+++ client/src/components/LoginPage/LoginForm.tsx
@@ -38,7 +38,7 @@ const LoginForm: React.FC<LoginFormProps> = ({ onSwitchToSignup }) => {
       <form className="login-form" onSubmit={handleLogin}>
         <input
           type="id"
-          placeholder="아이디를 입력하세요"
+          placeholder="이메일을 입력하세요"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
