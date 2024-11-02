0f703aebdb8a5f9c8ba0e35a9d4894971a99034a
seula <urghb12375@gmail.com>
2024-11-02 15:46:57 +0900

    feat-FE-SA/login 화면 kloudify  소개 문구 추가

--- client/src/components/LoginPage/IntroSection.tsx
+++ client/src/components/LoginPage/IntroSection.tsx
@@ -21,7 +21,25 @@ const IntroSection = () => {
 
   return (
     <>
-      <img src={loginImg} className="login-img" />
+      <div className="img-container">
+        <div className="gra-img">
+          <h3 className="title-start">
+            당신의 클라우드 비전,
+            <br /> 현실로 바꾸세요.
+          </h3>
+          <p className="title-middle">
+            Kloudify의 아키텍처 설계와 배포, 그 어떤 것도 어렵지 않습니다.
+            아이디어만 있으면 됩니다. Kloudify가 나머지를 처리하니까요.
+            자연스러운 대화만으로 복잡한 인프라를 설계하고, 서비스 간의 관계를
+            시각화해보세요. 한 번의 클릭으로 최적화된 클라우드 배포까지. 기술의
+            복잡함을 줄이고, 당신의 아이디어에만 집중하세요.
+          </p>
+          <div className="title-end">
+            아이디어에서 실행까지,
+            <br /> Kloudify로
+          </div>
+        </div>
+      </div>
       {/* <Slider {...settings} className="contents">
         {introData.map((slide) => (
           <div key={slide.id} className="intro-content">
