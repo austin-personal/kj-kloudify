12417df1a47bd563a8542bbc6cb210bc0c719a7d
changmo <kmaestro043@gmail.com>
2024-11-03 21:10:42 +0900

    test

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,5 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();  // CORS 활성화
-  await app.listen(3000, '0.0.0.0');
+  await app.listen(3000,'0.0.0.0');
+}
 bootstrap();
