5515866afc0a71cf8675d7940f8e2dbe69c9ae5b
seula <urghb12375@gmail.com>
2024-11-12 19:32:09 +0900

    feat-FE-SA/서비스 이미지 추가

--- client/src/utils/awsServices.js
+++ client/src/utils/awsServices.js
@@ -188,6 +188,18 @@ const serviceStateAliases = {
 
 const patternAliases = [
   {
+    keywords: ["application", "load"],
+    serviceName: "application-load-balancer",
+  },
+  {
+    keywords: ["acl"],
+    serviceName: "network-access-control-list",
+  },
+  {
+    keywords: ["table"],
+    serviceName: "route-table",
+  },
+  {
     keywords: ["security"],
     serviceName: "security-group",
   },
