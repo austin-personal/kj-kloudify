640f4ed4bc529b255c210074bca5a8503b469392
kim990401 <xogus1p@gmail.com>
2024-11-12 23:04:49 +0900

    feat-TH / redux-persist using

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
+import store, { persistor } from "./store/store";
 import App from "./App";
 import "./index.css";
 
@@ -12,9 +13,11 @@ const root = ReactDOM.createRoot(container!); // '!'는 null이 아님을 명시
 root.render(
   // <React.StrictMode>
   <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
+    <PersistGate loading={null} persistor={persistor}>
+      <Router>
+        <App />
+      </Router>
+    </PersistGate>
   </Provider>
   // </React.StrictMode>
 );
